355faead6d3dc76298bd8cb81fd76b81d5c071ee
unknown <g.andrey77@gmail.com>
2021-11-22 11:30:24 -0500

    barebones stream connected to backend

--- Frontend/src/components/Stream.jsx
+++ Frontend/src/components/Stream.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Stream.css'
 export default function Stream() {
+
+    const [url, setUrl] = useState("");
+    const [showStream, setShowStream] = useState(false)
+
+    const handleUrlChange = (e) => {
+        setUrl(e.target.value);
+        console.log(url)
+    };
+    
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            console.log('http://127.0.0.1:5000/video_feed?url='+url);
+            setShowStream(true);
+        }
+    }
+
     return (
-        <div className="stream" id="stream">
-            <h1>Pussy</h1>
-        </div>
+        <>
+            <div className="stream" id="stream">
+                <label>
+                    Youtube URL:
+                    <input type="text" value={url} onChange={handleUrlChange} id="youtube-url-field" onKeyDown={handleKeyDown}/>
+                </label>
+                {showStream ? 
+                    (
+                        <img src={'http://127.0.0.1:5000/video_feed?url='+url}  width="600" height="360"/>
+                    ) 
+                    : 
+                    (<></>)
+                }
+            </div>
+        </>
     )
 }
