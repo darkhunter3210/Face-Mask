d9ef9289cb7e7228df785a0998714452ab3fb8a5
unknown <g.andrey77@gmail.com>
2021-12-17 14:13:09 -0500

    frontpage text

--- Frontend/src/components/HeroSection.jsx
+++ Frontend/src/components/HeroSection.jsx
@@ -7,7 +7,7 @@ function HeroSection() {
   return (
     <div className='hero-container' id="intro">
       <video src='/videos/video-2.mp4' autoPlay loop muted />
-      <h1>Wear a mask, is your task</h1>
+      <h1>Wear a mask, it's your task</h1>
       <p>We will find you if you don't</p>
       <div className='hero-btns'>
         <Button
