e1cc6d65aef83c9871fd8528e4ab53289f3ff27e
Matthew Cheung <matthewcheung3003@gmail.com>
2021-12-20 17:59:06 -0500

    restyled the entired stream page

--- Frontend/src/components/Stream.jsx
+++ Frontend/src/components/Stream.jsx
@@ -18,6 +18,12 @@ export default function Stream() {
         }
     }
 
+    const handleClick = (event) =>
+    {
+        console.log('http://127.0.0.1:5000/video_feed?url='+url);
+        setShowStream(true);
+    }
+
     const handleStopbutton = (e) =>{
         axios.post('http://127.0.0.1:5000/stop_feed').then(res => console.log(res.data))
         console.log('pressed')
@@ -32,22 +38,32 @@ export default function Stream() {
     return (
         <>
             <div className="stream" id="stream">
-                <label>
-                    Youtube URL:
-                    <input type="text" value={url} onChange={handleUrlChange} id="youtube-url-field" onKeyDown={handleKeyDown}/>
-                    <button onClick={handleTest}>Test</button>
-                    
-                </label>
-                {/* <button class='stop' onClick={handleStopbutton}>Stop</button> */}
-                {showStream ? 
-                    ( <>
-                        <button class='stop' onClick={handleStopbutton}>Stop</button>
+                <div className="title">
+                    <h1>Stream Uploader</h1>
+                    <h3>Input any youtube stream</h3>
+                </div>
+                <div className="container"> 
+                    {/* <button class='stop' onClick={handleStopbutton}>Stop</button> */}
+                    {showStream ? 
+                    ( <div className="video-player">
                         <img src={'http://127.0.0.1:5000/video_feed?url='+url}  width="600" height="360"/>
-                    </>  
+                        <div className="button_local">
+                            <button class='stop' onClick={handleStopbutton}>Stop</button>
+                        </div>
+                    </div>
                     ) 
                     : 
-                    (<></>)
+                    (<form className="input-field">
+                    <input className="field-text" 
+                            type="text" value={url} 
+                            onChange={handleUrlChange} 
+                            id="youtube-url-field" 
+                            onKeyDown={handleKeyDown}
+                            placeholder="Enter your youtube stream URL here"/>
+                    <button onClick={handleClick}>Submit</button>
+                </form>)
                 }
+                </div>
             </div>
         </>
     )
